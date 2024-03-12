9adbbf4b9df1e057ad39b25f23bfb134a942395e
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2024-03-12 17:05:07 +0900

    edit TokenRepository

--- spring-jwt/src/main/java/com/example/springusermanagement/token/TokenRepository.java
+++ spring-jwt/src/main/java/com/example/springusermanagement/token/TokenRepository.java
@@ -11,5 +11,10 @@ import java.util.Optional;
 public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByToken(String token);
 
+    @Query(value = """
+      select t from Token t inner join UserEntity u\s
+      on t.user.id = u.id\s
+      where u.id = :id and (t.expired = false or t.revoked = false)\s
+      """)
     List<Token> findAllValidTokenByUser(Long id);
 }
