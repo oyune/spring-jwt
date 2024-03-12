cf8dde5a50a422db2dc86f1488a548da31b85d93
Chaemin Yooun <93826302+oyune@users.noreply.github.com>
2024-03-12 17:01:59 +0900

    add ApplicationConfig

---
+++ spring-jwt/src/main/java/com/example/springusermanagement/config/ApplicationConfig.java
@@ -0,0 +1,78 @@
+package com.example.springusermanagement.config;
+
+import com.example.springusermanagement.handler.JWTAccessDeniedHandler;
+import com.example.springusermanagement.handler.JWTAuthenticationEntryPoint;
+import com.example.springusermanagement.user.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+    private final JWTAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JWTAccessDeniedHandler jwtAccessDeniedHandler;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                // token 인증 방식을 사용하므로 csrf에 대한 보호 비활성화
+                .csrf(AbstractHttpConfigurer::disable)
+                // 세션을 사용하지 않으므로 stateless로 설정
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // 인증 및 접근 거부에 대한 핸들러 처리
+                .exceptionHandling(exceptionHandlingConfigurer ->
+                        exceptionHandlingConfigurer
+                                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                                .accessDeniedHandler(jwtAccessDeniedHandler)
+                )
+                // 회원가입, 로그인의 경우 별도의 인증 없이 접근할 수 있도록 허용
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/api/v1/auth/authenticate").permitAll() // 로그인 허용
+                                .requestMatchers("/api/v1/auth.register").permitAll() // 회원가입 허용
+                                .anyRequest().authenticated()
+                );
+
+        return httpSecurity.build();
+    }
+}
